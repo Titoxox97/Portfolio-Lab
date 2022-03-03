b6b5e3f6d9c9de792a2691e9a1cac54d82843675
Titoxox97 <87781987+Titoxox97@users.noreply.github.com>
2022-03-02 22:54:46 -0500

    components main/about/pwr

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { Route, Switch, useLocation } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { lightMode } from "./components/Themes";
+import GlobalStyle from "./globalStyles";
+
+// My web page components
+import Main from "./components/Main";
+import About from "./components/About";
+import BlogPage from "./components/BlogPage";
+import WorkStuff from "./components/WorkStuff";
+import Skills from "./components/Skills";
+
 function App() {
-  return <div>
-    App File
-    </div>
-    
+  const location = useLocation();
+  return (
+    <>
+      <GlobalStyle />
+
+      <ThemeProvider theme={lightMode}>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/blog" component={BlogPage} />
+          <Route exact path="/work" component={WorkStuff} />
+          <Route exact path="/skills" component={Skills} />
+        </Switch>
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default App
-
+export default App;
